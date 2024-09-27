6626b93fd531c73e34117967575b6f6d636b4dd4
embrana <67813424+embrana@users.noreply.github.com>
2024-09-27 19:40:05 +0000

    first attempt feature - wout style

--- src/app.js
+++ src/app.js
@@ -7,33 +7,37 @@ import "./assets/img/4geeks.ico";
 
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let simbols = [1, 2, 3, 4];
-let img = [];
+let img = ["♦", "♥", "♠", "♣"];
 
 function fNumbers(a) {
-  let aa = a;
-  console.log(aa);
+  console.log(a);
 }
+
 function fSimbols(a) {
-  let aa = a;
-  console.log(aa);
+  console.log(a);
 }
 
-function render() {
-  document.querySelector("#widget_content").innerHTML = `<div class="card"> 
-      <div class="simbol">simboltop</div>
-      <div class="number">number</div>
-      <div class="simbol">simbolbottom</div>
-      </div>
-      
-      `;
+function variables(a, b) {
+  let randomNumber = a[Math.floor(Math.random() * a.length)];
+  let randomSymbol = b[Math.floor(Math.random() * b.length)];
+  return {
+    number: randomNumber,
+    symbol: randomSymbol
+  };
 }
 
+let resultado = variables(numbers, simbols);
+console.log(resultado);
+
+function render(vars) {
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="card"> 
+      <div class="simbol">${img[vars.symbol]}</div>
+      <div class="number">${vars.number}</div>
+      <div class="simbol">${img[vars.symbol]}</div>
+    </div>
+  `;
+}
 window.onload = function() {
-  //write your code here
-  // console.log("Hello Rigo from the console!");
-
-  fNumbers("blah blah");
-  fSimbols("blah blah");
-
-  render();
+  render(resultado);
 };
