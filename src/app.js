f1a9b2ed5f5c8f7eef7ebd0eedd4f3d302958cb1
embrana <67813424+embrana@users.noreply.github.com>
2024-09-28 22:38:45 +0000

    full base func & style

--- src/app.js
+++ src/app.js
@@ -2,42 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 let simbols = [0, 1, 2, 3];
 let img = ["♦", "♥", "♠", "♣"];
 
-function fNumbers(a) {
-  console.log(a);
-}
-
-function fSimbols(a) {
-  console.log(a);
-}
-
-function variables(a, b) {
+let variables = (a, b) => {
   let randomNumber = a[Math.floor(Math.random() * a.length)];
   let randomSymbol = b[Math.floor(Math.random() * b.length)];
   return {
     number: randomNumber,
     symbol: randomSymbol
   };
-}
+};
 
 let resultado = variables(numbers, simbols);
-console.log(resultado);
+// console.log(resultado);
 
-function render(resultado) {
+let render = resultado => {
   document.querySelector("#widget_content").innerHTML = `
-    <div class="card"> 
+    <div class="card ${
+      simbols.indexOf(resultado.symbol) == "0" ? "diamond" : ""
+    }${simbols.indexOf(resultado.symbol) == "1" ? "heart" : ""}${
+    simbols.indexOf(resultado.symbol) == "2" ? "spade" : ""
+  }${simbols.indexOf(resultado.symbol) == "3" ? "club" : ""}"> 
       <div class="simbolt">${img[resultado.symbol]}</div>
       <div class="number">${resultado.number}</div>
       <div class="simbolb">${img[resultado.symbol]}</div>
     </div>
   `;
-}
+};
+
+document.querySelector("#widget_content").style.height =
+  window.innerHeight + "px";
+
 window.onload = function() {
   render(resultado);
 };
