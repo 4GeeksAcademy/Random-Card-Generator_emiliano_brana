c71a83216c695647f8c1cb1d3fa662a532bb1d63
embrana <67813424+embrana@users.noreply.github.com>
2024-09-29 02:41:37 +0000

    random card button and functionality

--- src/app.js
+++ src/app.js
@@ -6,21 +6,20 @@ let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 let simbols = [0, 1, 2, 3];
 let img = ["♦", "♥", "♠", "♣"];
 
-let variables = (a, b) => {
+function variables(a, b) {
   let randomNumber = a[Math.floor(Math.random() * a.length)];
   let randomSymbol = b[Math.floor(Math.random() * b.length)];
   return {
     number: randomNumber,
     symbol: randomSymbol
   };
-};
+}
 
 let resultado = variables(numbers, simbols);
-// console.log(resultado);
 
 let render = resultado => {
-  document.querySelector("#widget_content").innerHTML = `
-    <div class="card ${
+  document.querySelector("#card-display").innerHTML = `
+    <div class="card align-self-start ${
       simbols.indexOf(resultado.symbol) == "0" ? "diamond" : ""
     }${simbols.indexOf(resultado.symbol) == "1" ? "heart" : ""}${
     simbols.indexOf(resultado.symbol) == "2" ? "spade" : ""
@@ -29,7 +28,16 @@ let render = resultado => {
       <div class="number">${resultado.number}</div>
       <div class="simbolb">${img[resultado.symbol]}</div>
     </div>
-  `;
+`;
+};
+
+let renderbtn = () => {
+  document
+    .querySelector("#widget_content")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<button type="button" id="rndCard" class="btn btn-light ms-5">Random Card</button><button type="button" id="cardTime" class="btn btn-light ms-5">Card by time</button>`
+    );
 };
 
 document.querySelector("#widget_content").style.height =
@@ -37,4 +45,9 @@ document.querySelector("#widget_content").style.height =
 
 window.onload = function() {
   render(resultado);
+  renderbtn();
+
+  document.querySelector("#rndCard").addEventListener("click", function() {
+    render(variables(numbers, simbols));
+  });
 };
